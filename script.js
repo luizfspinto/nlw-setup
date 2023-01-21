3d68f5de49a7717f4d61950029c1ef907a4f099c
luizfspinto <luizfspintoo@gmail.com>
2023-01-21 10:00:00 -0300

    update with js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Hello World!")
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-03", "01-06"],
+  water: ["01-03"],
+  food: ["01-04"],
+  studies: ["01-02", "01-04"]
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
